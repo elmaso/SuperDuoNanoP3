28e676c51f4ae4620282c9273b63f0920a0f9d71
marioS <elmaso@gmail.com>
2015-10-01 21:04:03 -0700

    Starting Code
    Im restarting the code to have a better version control

--- alexandria/app/src/main/java/it/jaschke/alexandria/AddBook.java
+++ alexandria/app/src/main/java/it/jaschke/alexandria/AddBook.java
@@ -1,6 +1,7 @@
 package it.jaschke.alexandria;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -16,6 +17,8 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 import it.jaschke.alexandria.data.AlexandriaContract;
 import it.jaschke.alexandria.services.BookService;
@@ -87,9 +90,19 @@ public class AddBook extends Fragment implements LoaderManager.LoaderCallbacks<C
         rootView.findViewById(R.id.scan_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Start barcode
-                   Intent intent = new Intent(getActivity(), MultiTrackerActivity.class);
-                  startActivity(intent);
+                // This is the callback method that the system will invoke when your button is
+                // clicked. You might do this by launching another app or by including the
+                //functionality directly in this app.
+                // Hint: Use a Try/Catch block to handle the Intent dispatch gracefully, if you
+                // are using an external app.
+                //when you're done, remove the toast below.
+                Context context = getActivity();
+                CharSequence text = "This button should let you scan a book for its barcode!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+
             }
         });
 
